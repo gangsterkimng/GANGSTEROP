666e2068f6839a8f0fd21f0379b427268f4dce5a
gangseriskinmg <70527586+gangsterkimng@users.noreply.github.com>
2020-10-10 01:36:36 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -26,8 +26,8 @@ PM_ON_OFF = Config.PM_DATA
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 CUSTOM_MIDDLE_PMP = str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else "**YOU HAVE TRESPASSED TO MY MASTERS INBOX** \n`THIS IS ILLEGAL AND REGARDED AS A CRIME`"
 USER_BOT_WARN_ZERO = "`You were spamming my sweet master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot⭕️.`\n**Now GTFO, i'm busy**"
-USER_BOT_NO_WARN = ("`Hello, This is HellBot⚠️.You have found your way here to my master,`"
-                   f"{DEFAULTUSER}'s `inbox. Koi fayda nhi h re dekh... Accha khasa ghar me jake Roti,Bhaji bana yeh sab chale naa kar...\n"
+USER_BOT_NO_WARN = ("`Hello, This is GANGSTERBOT⚠️.You have found your way here to my master,`"
+                   f"{DEFAULTUSER}'s `inbox. Tumse na ho paega..  naa kar...\n"
                     "Leave your Name,Reason and 100 Million $ and hopefully you'll get a reply within 100 light years.`⭕️\n\n"
                     "❤️ Register Your Request! ❤️\nSend /start To Register Your Request!! 🔥\n"
                     "⭕️**Now You Are In Trouble So Send** 🔥 `/start` 🔥 **To Start A Valid Conversation!!**⭕️")
