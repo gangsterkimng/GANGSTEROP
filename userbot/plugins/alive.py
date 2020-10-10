7a051354a12620fa63101ddc9b8af8aa405da3bf
gangseriskinmg <70527586+gangsterkimng@users.noreply.github.com>
2020-10-10 13:17:40 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,18 +7,18 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
 PM_IMG = "https://telegra.ph/file/4b370450811f0cb8a23ac.jpg"
 pm_caption = "🔱 **GANGSTER Is Online** 🔱\n\n"
 
-pm_caption += f"🔸🔹 **MY GOD**           :   {DEFAULTUSER}\n"
+pm_caption += f"🔸🔹 **MERE BHAGWAAN**           :   {DEFAULTUSER}\n"
 
 pm_caption += "🔹🔸 тєℓєтнσи νєяѕισи   :   1.15.0 \n"
 
-pm_caption += "🔸🔹 σffι¢ιαℓ ¢нαииєℓ   :   [ᴊᴏɪɴ](https://www.pornhub.org/desi)\n"
+pm_caption += "🔸🔹 σffι¢ιαℓ ¢нαииєℓ   :   [ᴊᴏɪɴ](https://t.me/GANGSTERSUPPORT)\n"
 
-pm_caption += "🔹🔸 σffι¢ιαℓ gяσυρ     :   [ᴊᴏɪɴ](https://www.pornhub.org/videsi)\n"
+pm_caption += "🔹🔸 σffι¢ιαℓ gяσυρ     :   [ᴊᴏɪɴ](https://t.me/GANGSTERSUPPORT)\n"
 
 
 pm_caption += "🔹🔸 ¢σρуяιgнт          :   [GANGSTERBOT-OWNER](https://t.me/ALONGANGSTER)\n"
 
-pm_caption += " [...▄███▄███▄\n....█████████\n.......▀█████▀\n............▀█▀\n](https://t.me/ALONGANGSTER)\n"
+pm_caption += " [...°°°°°°°°°\n....°°°°°°°°\n.......°°°°°°\n............°°°\n](https://t.me/ALONGANGSTER)\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
